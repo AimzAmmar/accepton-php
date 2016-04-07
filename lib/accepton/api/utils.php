605b95252db6a33e89219fbac8d7b6530a495d77
Michael Herold <michael.j.herold@gmail.com>
2016-04-07 17:41:38 -0500

    Remove unused helper

--- lib/accepton/api/utils.php
+++ lib/accepton/api/utils.php
@@ -4,12 +4,6 @@ namespace AcceptOn;
 
 trait Utils
 {
-    public static function endsWith($haystack, $needle)
-    {
-        // search forward starting from end minus needle length characters
-        return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
-    }
-
     public static function startsWith($haystack, $needle)
     {
         // search backwards starting from haystack length characters from the end
